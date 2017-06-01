e0e50709d2420e6912979f21f98be3dfbd827c74
Magda Sudak <cmagdalenaa@gmail.com>
2017-06-01 15:03:54 +0200

    uporzadkowanie tresci

--- check-check/resources/views/master.blade.php
+++ check-check/resources/views/master.blade.php
@@ -46,45 +46,46 @@
         }
     </style>
 </head>
-<body id="app-layout">
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <section id="banner" class="banner">
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="#">
-                    <div class="banner-logo text-center">
-                        <img src="img/logo.jpg" class="img-responsive">
-                    </div> 
-                </a>
-            </div>
+           
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="">Start</a></li>
-                    <li><a href="articles.html">Projekty</a></li>
-                    <li><a href="/contact">Kontakt</a></li>
-                    <li><a href="/about">O nas</a></li>
-                </ul>
+              <nav class="navbar navbar-default navbar-fixed-top">
+                              <div class="container">
+                                <div class="col-md-12">
+                                    <div class="navbar-header">
+                                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                      </button>
+                                      <!--<a class="navbar-brand" href="#"><img src="img/logo.jpg" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>-->
+                                    </div>
+                                    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+                                      <ul class="nav navbar-nav">
+                                        <li class="active"><a href="#banner">Start</a></li>
+                                        <li class=""><a href="#service">Usługi</a></li>
+                                        <li class=""><a href="#about">O nas</a></li>
+                                        <li><a href="articles.html">Projekty</a></li>
+                                        <!--<li class=""><a href="#testimonial">Testimonial</a></li>-->
+                                        <li class=""><a href="#contact">Kontakt</a></li>
+                                        <li><a href="login">Zaloguj</a></li>
+                                        <li><a href="register">Rejestracja</a></li>
+                                      </ul>
+                                    </div>
+                                </div>
+                              </div>
+                </nav>       
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                        <li><a href="login">Zaloguj</a></li>
-                        <li><a href="register">Rejestracja</a></li>
-                </ul>
+               
             </div>
         </div>        
     </nav>
+    </section>
 
     <!-- wrapper -->
     <div class="site-wrappper">
@@ -97,12 +98,59 @@
     </div><!-- end of wrapper -->
     
     <menu></menu>
-    <!-- Footer -->
-    <footer class="site-footer">
-        <div class="container">
-            <p>&copy; Check-check</p>
-        </div>
-    </footer>
+    <!--footer-->
+                    <footer id="footer">
+                        <div class="top-footer">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-4 col-sm-4 marb20">
+                                            <div class="ftr-tle">
+                                                <h4 class="white no-padding">O nas</h4>
+                                            </div>
+                                            <div class="info-sec">
+                                                <p>Praesent convallis tortor et enim laoreet, vel consectetur purus latoque penatibus et dis parturient.</p>
+                                            </div>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 marb20">
+                                        <div class="ftr-tle">
+                                            <h4 class="white no-padding">Szybkie linki</h4>
+                                        </div>
+                                        <div class="info-sec">
+                                            <ul class="quick-info">
+                                                <li><a href="index.html"><i class="fa fa-circle"></i>Start</a></li>
+                                                <li><a href="#service"><i class="fa fa-circle"></i>Usługi</a></li>
+                                                <li><a href="#contact"><i class="fa fa-circle"></i>Kontakt</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 marb20">
+                                        <div class="ftr-tle">
+                                            <h4 class="white no-padding">Znajdź nas</h4>
+                                        </div>
+                                        <div class="info-sec">
+                                            <ul class="social-icon">
+                                                <li class="bglight-blue"><i class="fa fa-facebook"></i></li>
+                                                <li class="bgred"><i class="fa fa-google-plus"></i></li>
+                                                <li class="bgdark-blue"><i class="fa fa-linkedin"></i></li>
+                                                <li class="bglight-blue"><i class="fa fa-twitter"></i></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer-line">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        © Copyright Check-check. All Rights Reserved
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                    <!--/ footer-->
 
 
     <!-- JavaScripts -->
